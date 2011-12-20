309348b92f5923a7b9170de2a8fcc3f98f0cfa63
Chris Kelly <ckdake@ckdake.com>
2011-12-20 11:18:31 -0500

    [Finish #22500839] Update fakeweb output to match api

--- test/integration/mandrill_test.rb
+++ test/integration/mandrill_test.rb
@@ -6,12 +6,12 @@ class MandrillIntegrationTest < Test::Unit::TestCase
     FakeWeb.register_uri(
       :post,
       'https://mandrillapp.com/api/1.0/users/ping',
-      body: "Everything's Chimpy!"
+      body: "PONG!"
     )
     expected_request = "key=abc123-us1&options[track_opens]=true&options[track_clicks]=true"
     
     m = Mailchimp::Mandrill.new('abc123-us1')
-    assert_equal "Everything's Chimpy!", m.users_ping
+    assert_equal "PONG!", m.users_ping
     assert_equal expected_request, URI::decode(FakeWeb.last_request.body)
   end
 
